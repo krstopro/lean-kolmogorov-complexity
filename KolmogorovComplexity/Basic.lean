ef011b74457c1d05bccb83549385e981121eafcb
Izan Beltran <izanbf1803@gmail.com>
2025-12-11 19:05:30 +0100

    bring back defs/lemmas removed by mistake

--- KolmogorovComplexity/Basic.lean
+++ KolmogorovComplexity/Basic.lean
@@ -1,4 +1,5 @@
 import Mathlib.Computability.Partrec
+import Mathlib.Computability.PartrecCode
 import Mathlib.Data.Nat.Basic
 import Mathlib.Data.Part
 import Mathlib.Data.PFun
@@ -8,7 +9,7 @@ import Mathlib.Tactic
 
 namespace KolmogorovComplexity
 
-open Part Set
+open Part Set Encodable
 
 variable {S : Type*}
 variable [Primcodable S]
@@ -21,7 +22,7 @@ def l (n : ℕ) : ℕ := Nat.log2 (n + 1)
   Cf(x) = min { l(p) : f(p) = n(x) }
 -/
 noncomputable def complexity (f: ℕ →. ℕ) (x : S) :=
-  sInf ((fun p => (l p : WithTop ℕ)) '' { p | Encodable.encode x ∈ f p })
+  sInf ((fun p => (l p : WithTop ℕ)) '' { p | encode x ∈ f p })
 
 def minorizes (f g : ℕ →. ℕ) : Prop :=
   ∃ c : ℕ, ∀ x : S, complexity f x ≤ complexity g x + c
@@ -49,18 +50,49 @@ def additively_optimal (f : ℕ →. ℕ) (C : Set (ℕ →. ℕ)) : Prop :=
 
 /-- There is an additively optimal universal partial computable function. -/
 lemma lem_2_1_1 : ∃ f : ℕ →. ℕ, Partrec f ∧ additively_optimal (S := S) f { g : ℕ →. ℕ | Partrec g } := by
+  use
+    let code_left := Nat.Partrec.Code.comp Nat.Partrec.Code.left Nat.Partrec.Code.id
+    let code_right := Nat.Partrec.Code.comp Nat.Partrec.Code.right Nat.Partrec.Code.id
+    let code := Nat.Partrec.Code.pair code_left code_right
+    Nat.Partrec.Code.eval code
+  constructor
+  · simp
+    constructor
+    refine Nat.Partrec.pair ?_ ?_
+    · apply Nat.Partrec.Code.exists_code.mpr
+      use Nat.Partrec.Code.left.comp Nat.Partrec.Code.id
+    · apply Nat.Partrec.Code.exists_code.mpr
+      use (Nat.Partrec.Code.right.comp Nat.Partrec.Code.id)
+    · apply Nat.Partrec.Code.exists_code.mpr
+      simp
+      use Nat.Partrec.Code.id
+      funext n
+      exact Nat.Partrec.Code.eval_id n
+  · unfold additively_optimal
+    intro g g_mem
+    unfold minorizes
+    sorry
+
+def description (φ : ℕ →. ℕ) (x y: S) (p : ℕ ) : Prop :=
+  Partrec φ ∧ φ (Nat.pair (encode y) p) = encode x
+
+noncomputable def description_complexity (φ: ℕ →. ℕ) (x y : S) :=
+  sInf ((fun p => (l p : WithTop ℕ)) '' { p | description φ x y p })
+
+def description_minorizes (φ γ : ℕ →. ℕ) : Prop :=
+  ∃ χ : ℕ, ∀ x y : S, description_complexity φ x y ≤ description_complexity γ x y + χ
+
+def description_additively_optimal (φ₀ : ℕ →. ℕ) (C : Set (ℕ →. ℕ)) : Prop :=
+  ∀ φ ∈ C, description_minorizes (S := S) φ₀ φ
+
+theorem the_2_1_1 : ∃ φ₀ : ℕ →. ℕ, Partrec φ₀ ∧ description_additively_optimal (S:= S) φ₀ {φ : ℕ →. ℕ | Partrec φ} :=
   sorry
 
-def description (f : ℕ →. ℕ) (x y: S) (p : ℕ ) : Prop :=
-  Partrec f ∧ f (Nat.pair (Encodable.encode y) p) = Encodable.encode x
-
-noncomputable def description_complexity (f: ℕ →. ℕ) (x y : S) :=
-  sInf ((fun p => (l p : WithTop ℕ)) '' { p | Encodable.encode x ∈ f (Nat.pair (Encodable.encode y) p) })
-
-def φ₀ : ℕ →. ℕ := sorry
+noncomputable def φ₀ :=
+  Classical.choose (the_2_1_1 (S:=S))
 
 noncomputable def conditional_complexity (x y : S) :=
-  description_complexity φ₀ x y
+  description_complexity (φ₀ (S := S)) x y
 
 noncomputable def unconditional_complexity (x : S) := by
   choose ε _ _ using Denumerable.decode_inv (α := S) 0
